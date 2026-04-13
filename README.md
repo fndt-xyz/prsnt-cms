70ca64cd475c87a51dff457a189cda3841276a1d
fndt-xyz <fndt.xyz@gmail.com>
2026-04-13 22:16:18 +0700

    v0.01

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # prsnt-cms
 publications for prnst-cms
+
+Quick Start
+Clone and Install:
+
+```bash
+
+git clone https://github.com/fndt-xyz/prsnt-cms.git
+
+cd prsnt-cms
+
+./start.sh
+
+xdg-open http://localhost:8085
+
+```
--- start.sh
+++ start.sh
@@ -6,4 +6,4 @@ fuser -k 8085/tcp > /dev/null 2>&1
 nohup python3 -m http.server 8085 > /dev/null 2>&1 &
 
 echo "Server started at http://localhost:8085"
-echo "Process running in background. Use 'fuser -k 8084/tcp' to stop it."
+echo "Process running in background. Use 'fuser -k 8085/tcp' to stop it."
